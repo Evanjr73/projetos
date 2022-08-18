766cba96ae56f7d7486be0536c167328e4c83a91
Evan junior <evsioljr@gmail.com>
2022-08-17 21:58:03 -0300

    descrição das casas

--- casadehoguards/index.js
+++ casadehoguards/index.js
@@ -1,7 +1,29 @@
-function textto(){
-    
-    
-   let sonserina =  document.getElementById("footer")
-   sonserina.innerHTML="teste"
-
+function clicou(){
+   
+   let descricao = document.getElementById("footer").innerHTML = "Grifinória. Lar dos bravos e cavalheiros, o símbolo da Grifinória apresenta em seu estandarte um orgulhoso leão, acompanhado das cores vermelha e amarelo-dourada. A Casa valoriza feitos de coragem e superação pessoal que costumam ser realizados por altruísmo.";
+   return
+   
 }
+    
+function clicou2(){
+   
+   let descricao = document.getElementById("footer").innerHTML = "A Lufa-Lufa é a mais inclusiva das quatro casas e valoriza o trabalho duro, a paciência, a lealdade e o jogo limpo em vez de aptidão em seus membros. Seu animal símbolo é o texugo, e suas cores são o amarelo e o preto. A Lufa-Lufa corresponde ao elemento Terra.";
+   return
+   
+}
+function clicou3(){
+   
+   let descricao = document.getElementById("footer").innerHTML = "Corvinal. Os bruxos pertencentes à Corvinal se destacam por sua exímia inteligência. Nesse contexto, suas principais características são relacionadas a sabedoria, criatividade e personalidade única, o que garante a eles certa excentricidade.";
+   return
+   
+}
+function clicou4(){
+   
+   let descricao = document.getElementById("footer").innerHTML = "Os membros da Sonserina têm como características, sobretudo, a ambição e a busca pela grandeza, por isso estão sempre se dedicando a alcançar e expandir metas e se empenhando com inteligência em diversas áreas da magia.";
+   return
+   
+}
+     
+
+
+// 
